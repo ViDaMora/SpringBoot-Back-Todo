59e7dbf153c68f1f79ced45a0c83d1dfde2ca419
Victor Mora <vdaniel.mora@udea.edu.co>
2021-05-31 17:46:14 -0500

    Inicializacion y implementecion del servidor, desarrollo de los endpoints

---
+++ src/main/java/co/com/sofkau/demo/CrudAppication.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppication.class, args);
+	}
+
+}
---
+++ src/main/java/co/com/sofkau/demo/TodoRepository.java
@@ -0,0 +1,8 @@
+package co.com.sofkau.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TodoRepository extends CrudRepository<Todo,Long> {
+
+
+}
